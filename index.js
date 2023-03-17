31a684d86c4aba10f35f0cd0fa94858acaa8ef18
supercoderlee <lizhen118116@163.com>
2023-03-17 18:56:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,132 @@
+class PyModule {
+    constructor(addon, pModule) {
+        this._addon = addon;
+        this._pModule = pModule;
+    }
+
+    /**
+     * sync call python func
+     * @param {*} func 
+     * @param {*} args 
+     * @returns 
+     */
+    callSync = (func, args) => {
+        return this._addon.callSync(this._pModule, func, args);
+    }
+
+    /**
+     * async call python func
+     * @param {*} func 
+     * @param {*} args 
+     * @param {*} okCallBack callback on ok.
+     * @param {*} errorCallBack callback on error.
+     */
+    call = (func, args, okCallBack = null, errorCallBack = null) => {
+        this._addon.call(this._pModule, func, args, okCallBack, errorCallBack);
+    }
+}
+
+class PyRunner {
+    constructor() {
+        /* pyrunner init config */
+        // node-pyrunner install in ./node_modules/node-pyrunner
+        let appHome = __dirname.replace('\\node_modules\\node-pyrunner', '').replace('/node_modules/node-pyrunner', '');
+        this.config = {
+            "python_version": "3.10.10",
+            "python_home": `${appHome}/python/${process.platform}/${process.arch}/3.10.10`,
+            "program_name": process.platform == 'win32' ? 'python' : 'python3',
+            "base_prefix": "",
+            "base_exec_prefix": "",
+            "base_executable": "",
+            "prefix": `${appHome}/pyscript/venv`,
+            "exec_prefix": `${appHome}/pyscript/venv`,
+            "executable": `${appHome}/pyscript/venv/Scripts/python${process.platform == 'win32' ? '.exe' : ''}`,
+            "pythonpath_env": `${appHome}/pyscript/venv/Lib/site-packages`,
+            "module_search_paths": [__dirname, appHome, `${appHome}/pyscript`],
+            "encoding": "utf-8"
+        }
+        /* pyrunner addon object */
+        this._addon = require(`./addons/${this.config['python_version']}/${process.platform}/${process.arch}/pyrunner`);
+        /* init state */
+        this._isInit = false;
+    }
+
+    /**
+     * pyrunner init method
+     * @returns 
+     */
+    init = () => {
+        this.config['base_prefix'] = this.config['base_prefix'] == "" ? this.config['python_home'] : this.config['base_prefix'];
+        this.config['base_exec_prefix'] = this.config['base_exec_prefix'] == "" ? this.config['python_home'] : this.config['base_exec_prefix'];
+        this.config['base_executable'] = this.config['base_executable'] == "" ? this.config['python_home'] : this.config['base_executable'];
+        this._isInit = this._addon.init(this.config);
+        return this._isInit;
+    }
+
+    /**
+     * sync run python script
+     * @param {*} pyScript 
+     * @returns 
+     */
+    runScriptSync = (pyScript) => {
+        if (this._isInit) {
+            this._addon.runScriptSync(pyScript);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * async run python script
+     * @param {*} pyScript python script.
+     * @param {*} okCallBack callback on ok.
+     * @param {*} errorCallBack callback on error
+     * @returns 
+     */
+    runScript = (pyScript, okCallBack = null, errorCallBack = null) => {
+        if (this._isInit) {
+            this._addon.runScript(pyScript, okCallBack, errorCallBack);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * create python module object
+     * @param {*} pModule 
+     * @returns 
+     */
+    loadModule = (pModule) => {
+        if (this._isInit) {
+            let pyModule = new PyModule(this._addon, pModule);
+            return pyModule;
+        }
+    }
+
+    /**
+     * release pyrunner
+     * @returns 
+     */
+    release = () => {
+        if (this._isInit) {
+            this._addon.release();
+            return true;
+        }
+        return false;
+    }
+
+    about = () => {
+        if (this._isInit) {
+            this._addon.about();
+            return true;
+        }
+        return false;
+    }
+
+    version = () => {
+        return '1.0.0';
+    }
+
+}
+
+module.exports = new PyRunner()
